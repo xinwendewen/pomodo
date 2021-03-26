02db60a31fc57025aaf75ce49081a76f89ad551c
xinwendewen <zwt0909@outlook.com>
2021-03-27 00:23:20 +0800

    add reset and toggle button

--- app/src/main/java/com/xinwendewen/android/pomodo/PomoWidget.kt
+++ app/src/main/java/com/xinwendewen/android/pomodo/PomoWidget.kt
@@ -1,24 +1,54 @@
 package com.xinwendewen.android.pomodo
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.os.SystemClock
+import android.util.Log
 import android.widget.RemoteViews
+import java.lang.UnsupportedOperationException
 
-/**
- * Implementation of App Widget functionality.
- */
+const val CLICK_ACTION = "com.xinwendewen.android.pomodo.CLICK"
+const val RESET_ACTION = "com.xinwendewen.android.pomodo.RESET"
+private const val TAG = "PomoWidget"
+var isRunning = true
+
 class PomoWidget : AppWidgetProvider() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        when (intent?.action) {
+            CLICK_ACTION -> {
+                val widgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
+                if (widgetId == -1) {
+                    Log.w(TAG, "no appwidget id")
+                } else {
+                    val appWidgetManager = AppWidgetManager.getInstance(context)
+                    toggleTimer(context, appWidgetManager, widgetId)
+                }
+            }
+            RESET_ACTION -> {
+                val widgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, -1)
+                if (widgetId == -1) {
+                    Log.w(TAG, "no appwidget id")
+                } else {
+                    val appWidgetManager = AppWidgetManager.getInstance(context)
+                    initTimer(context, appWidgetManager, widgetId)
+                }
+            }
+        }
+        super.onReceive(context, intent)
+    }
+
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+        if (appWidgetIds.size != 1) {
+            throw UnsupportedOperationException("not support multiple host")
         }
+        initTimer(context, appWidgetManager, appWidgetIds[0])
     }
 
     override fun onEnabled(context: Context) {
@@ -30,16 +60,58 @@ class PomoWidget : AppWidgetProvider() {
     }
 }
 
-internal fun updateAppWidget(
+internal fun initTimer(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.pomo_widget).apply {
-        setChronometer(R.id.pomo_timer, SystemClock.elapsedRealtime() + 60 * 1000 * 25, null, true)
+    val views = createRemoteTimer(context).apply {
+        setChronometer(
+            R.id.pomo_timer,
+            SystemClock.elapsedRealtime() + 60 * 1000 * 25,
+            null,
+            false
+        )
+        setOnClickPendingIntent(
+            R.id.toggle,
+            PendingIntent.getBroadcast(
+                context,
+                0,
+                Intent(context, PomoWidget::class.java).apply {
+                    action = CLICK_ACTION
+                    putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                },
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        )
+        setOnClickPendingIntent(
+            R.id.reset,
+            PendingIntent.getBroadcast(
+                context,
+                0,
+                Intent(context, PomoWidget::class.java).apply {
+                    action = RESET_ACTION
+                    putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+                },
+                PendingIntent.FLAG_UPDATE_CURRENT
+            )
+        )
     }
-
-    // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
+
+internal fun toggleTimer(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val views = createRemoteTimer(context).apply {
+        setBoolean(R.id.pomo_timer, "setStarted", isRunning)
+        isRunning = !isRunning
+    }
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
+
+
+internal fun createRemoteTimer(context: Context) =
+    RemoteViews(context.packageName, R.layout.pomo_widget)
